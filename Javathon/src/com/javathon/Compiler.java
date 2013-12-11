da3db3a9053596cde711a76ec1199b616f672c66
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2013-12-11 12:33:38 +0800

    Initial commit.

---
+++ Javathon/src/com/javathon/Compiler.java
@@ -0,0 +1,13 @@
+package com.javathon;
+
+public class Compiler
+{
+    /**
+     * The despicable function that is so long and difficult
+     * for newbies in Java to understand.
+     * @param args
+     */
+    public static void main(String args[]){
+        System.out.println("Magandang bati, mundo!");
+    }
+}
