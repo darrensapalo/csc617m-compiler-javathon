89f688a347eb743f07f69b27a00c0181bb944426
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2013-12-15 17:04:22 +0800

    Using AntlrFileStream instead.

--- Javathon/src/com/javathon/Compiler.java
+++ Javathon/src/com/javathon/Compiler.java
@@ -1,12 +1,9 @@
 package com.javathon;
 
-import java.io.File;
-import java.util.Scanner;
-
 import grammar.JavathonLexer;
 import grammar.JavathonParser;
 
-import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CommonTokenStream;
 
 
@@ -18,17 +15,9 @@ import org.antlr.runtime.CommonTokenStream;
  */
 public class Compiler {
 	public static void main(String[] args) throws Exception {
-		// the input source
-		String source = "Darren is x = 52 but what";
-		File f = new File("test.txt");
-		Scanner s = new Scanner(f);
-		
-		StringBuilder sb = new StringBuilder();
-		while(s.hasNext())
-			sb.append(s.nextLine());
 		
 		// create an instance of the lexer
-		JavathonLexer lexer = new JavathonLexer(new ANTLRStringStream(sb.toString()));
+		JavathonLexer lexer = new JavathonLexer(new ANTLRFileStream("test.txt"));
 
 		// wrap a token-stream around the lexer
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
