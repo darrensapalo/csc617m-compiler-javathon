c8f0f4bf94ba005af78b11410a5cc18808e8a4f0
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2013-12-15 16:58:23 +0800

    Testing file input and parsing.

--- Javathon/src/com/javathon/Compiler.java
+++ Javathon/src/com/javathon/Compiler.java
@@ -1,5 +1,8 @@
 package com.javathon;
 
+import java.io.File;
+import java.util.Scanner;
+
 import grammar.JavathonLexer;
 import grammar.JavathonParser;
 
@@ -10,9 +13,15 @@ public class Compiler {
 	public static void main(String[] args) throws Exception {
 		// the input source
 		String source = "Darren is x = 52 but what";
-
+		File f = new File("test.txt");
+		Scanner s = new Scanner(f);
+		
+		StringBuilder sb = new StringBuilder();
+		while(s.hasNext())
+			sb.append(s.nextLine());
+		
 		// create an instance of the lexer
-		JavathonLexer lexer = new JavathonLexer(new ANTLRStringStream(source));
+		JavathonLexer lexer = new JavathonLexer(new ANTLRStringStream(sb.toString()));
 
 		// wrap a token-stream around the lexer
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
