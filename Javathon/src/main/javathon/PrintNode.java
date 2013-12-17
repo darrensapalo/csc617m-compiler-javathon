954003d742a8a88d9fcb61fd85b406dc199d7c8f
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2013-12-17 17:03:54 +0800

    Allowed print and println to have no parameters.

--- Javathon/src/main/javathon/PrintNode.java
+++ Javathon/src/main/javathon/PrintNode.java
@@ -1,27 +1,31 @@
 package main.javathon;
 
-import java.io.PrintStream;  
+import java.io.PrintStream;
+
 /*
  * Handles an output stream.
  */
-public class PrintNode implements JNode {  
-  
-  private JNode expression;  
-  private PrintStream out;  
-  
-  public PrintNode(JNode e) {  
-    this(e, System.out);  
-  }  
-  
-  public PrintNode(JNode e, PrintStream o) {  
-    expression = e;  
-    out = o;  
-  }  
-  
-  @Override  
-  public JValue evaluate() {  
-    JValue value = expression.evaluate();  
-    out.print(value);  
-    return JValue.VOID;  
-  }  
-}  
+public class PrintNode implements JNode {
+
+	private JNode expression;
+	private PrintStream out;
+
+	public PrintNode(JNode e) {
+		this(e, System.out);
+	}
+
+	public PrintNode(JNode e, PrintStream o) {
+		expression = e;
+		out = o;
+	}
+
+	@Override
+	public JValue evaluate() {
+		if (expression == null)
+			return JValue.VOID;
+		JValue value = expression.evaluate();
+
+		out.print(value);
+		return JValue.VOID;
+	}
+}
--- Javathon/src/main/javathon/PrintlnNode.java
+++ Javathon/src/main/javathon/PrintlnNode.java
@@ -1,28 +1,34 @@
 package main.javathon;
 
-import java.io.PrintStream;  
+import java.io.PrintStream;
+
 /**
  * Handles an output stream.
+ * 
  * @author Darren
  */
-public class PrintlnNode implements JNode {  
-  
-  private JNode expression;  
-  private PrintStream out;  
-  
-  public PrintlnNode(JNode e) {  
-    this(e, System.out);  
-  }  
-  
-  public PrintlnNode(JNode e, PrintStream o) {  
-    expression = e;  
-    out = o;  
-  }  
-  
-  @Override  
-  public JValue evaluate() {  
-    JValue value = expression.evaluate();  
-    out.println(value);  
-    return JValue.VOID;  
-  }  
-}  
+public class PrintlnNode implements JNode {
+
+	private JNode expression;
+	private PrintStream out;
+
+	public PrintlnNode(JNode e) {
+		this(e, System.out);
+	}
+
+	public PrintlnNode(JNode e, PrintStream o) {
+		expression = e;
+		out = o;
+	}
+
+	@Override
+	public JValue evaluate() {
+		if (expression == null){
+			out.println();
+			return JValue.VOID;
+		}
+		JValue value = expression.evaluate();
+		out.println(value);
+		return JValue.VOID;
+	}
+}
